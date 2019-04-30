0cee5439feef9e1b521a6b990606e0915401ae1e
Divyendu Shekhar <divyendua@gmail.com>
2019-04-30 13:35:44 +0530

    test jquery support

--- script/index.js
+++ script/index.js
@@ -1,10 +1,15 @@
-$(".bottom-container").hide()
-$(".middle-container").hide()
+// $(".bottom-container").hide()
+// $(".middle-container").hide()
 
-// scroll text animation
+// // scroll text animation
 
-var scrollAnimate = function() {
-    $(".scroll").animate({bottom: 20});
-    $(".scroll").animate({bottom: 40});
-};
-setInterval(scrollAnimate, 1000);
+// var scrollAnimate = function() {
+//     $(".scroll").animate({bottom: 20});
+//     $(".scroll").animate({bottom: 40});
+// };
+// setInterval(scrollAnimate, 1000);
+
+if (typeof jQuery != 'undefined')
+{ // jQuery is loaded => print the version
+    alert(jQuery.fn.jquery);
+}
