c1dddb65640b9ab80448254a63e140ca75950cd8
Divyendu Shekhar <divyendua@gmail.com>
2019-05-03 01:01:23 +0530

    commit

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-$(".bottom-container").hide()
-$(".middle-container").hide()
+$(".bottom-container").hide();
+$(".middle-container").hide();
 
 // scroll text animation
 
@@ -9,8 +9,20 @@ var scrollAnimate = function() {
 };
 setInterval(scrollAnimate, 1000);
 
+// moving page
+x=0;
 document.querySelector("body").addEventListener("wheel", myFunction);
-
-function myFunction() {
-  console.log("hola")
-}
+function myFunction(e) {
+  if (e.deltaY < 0) {
+    x-=100;
+    $(".hello").animate({
+      width: '300px'
+    });
+  }
+  if (e.deltaY > 0) {
+    x+=100;
+    $(".hello").animate({
+      width: '300px'
+    });
+  }
+};
