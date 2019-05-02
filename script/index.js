b2e015d5f874c5a548c1c2f1595978f17baf44c8
Divyendu Shekhar <divyendua@gmail.com>
2019-05-02 23:07:17 +0530

    commit

--- script/index.js
+++ script/index.js
@@ -1,15 +1,10 @@
-// $(".bottom-container").hide()
-// $(".middle-container").hide()
+$(".bottom-container").hide()
+$(".middle-container").hide()
 
-// // scroll text animation
+// scroll text animation
 
-// var scrollAnimate = function() {
-//     $(".scroll").animate({bottom: 20});
-//     $(".scroll").animate({bottom: 40});
-// };
-// setInterval(scrollAnimate, 1000);
-
-if (typeof jQuery != 'undefined')
-{ // jQuery is loaded => print the version
-    alert(jQuery.fn.jquery);
-}
+var scrollAnimate = function() {
+    $(".scroll").animate({bottom: 20});
+    $(".scroll").animate({bottom: 40});
+};
+setInterval(scrollAnimate, 1000);
