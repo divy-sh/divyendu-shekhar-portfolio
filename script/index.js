485e10b5f66dfdcb6f8a755c1b6a30521b2e34b5
Divyendu Shekhar <divyendua@gmail.com>
2019-05-08 03:04:31 +0530

    .

--- script/index.js
+++ script/index.js
@@ -10,7 +10,6 @@ setInterval(scrollAnimate, 1000);
 var loc = 0
 var prev_loc = 0
 document.addEventListener("wheel", track_movement);
-document.onscroll(track_movement);
 
 function track_movement(e)
 {
