d94008f729f0c012e8a0e6aebf2bdbecffd761b5
Divyendu Shekhar <divyendua@gmail.com>
2019-05-10 00:12:48 +0530

    .

--- script/index.js
+++ script/index.js
@@ -1,128 +1,38 @@
+
+var loc = 0
+var prev_loc = 0
+
+
 // scroll text animation
-$(document).ready(function(){
-    document.querySelector('.top-container').scrollIntoView();
-    $(".top-container").slideDown();
-    $(".scroll").fadeIn();
-    $(".name").slideDown();
-    $(".tech").slideDown();
-    $(".webdev").slideDown();
-    $(".engineer").slideDown();
-    $(".ai").slideDown();
-    $(".middle-container").slideDown();
-});
+
 var scrollAnimate = function() {
     $(".scroll").animate({bottom: 10});
     $(".scroll").animate({bottom: 20});
 };
 setInterval(scrollAnimate, 1000);
 
-// moving page
+//details change
 
-var loc = 0
-var prev_loc = 0
-
-//Touch
-
-$(document).swipe( {
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-    if(direction=="up"){
-        update_loc(+1);
+$(document).scroll(track_movement)
+function track_movement(){
+    var scroll = $(document).scrollTop();
+    if(scroll!=0){
+        $(".scroll").fadeOut();
     }
-    else if(direction=="down"){
-        update_loc(-1);
+    if(scroll==0){
+        $(".scroll").fadeIn();
     }
-    },
-    threshold:0
-});
-
-//Scrollwheel
-
-document.addEventListener("wheel", track_movement);
-function track_movement(e){
-    if (e.deltaY < 0){
-        update_loc(-1);
+    if(scroll>=0 & scroll<50){
+        $(".name").text("I am Divyendu");
     }
-    if (e.deltaY > 0){
-        update_loc(+1);
+    else if(scroll>=50 & scroll<200){
+        $(".name").text("Tech Enthusiast");
     }
-}
-
-//keys
-
-$(document).keydown(function(e){
-    console.log(e.key)
-    if(e.key=="ArrowDown" || e.key=="PageDown"){
-        update_loc(+1);
+    else if(scroll>=200 & scroll<350){
+        $(".name").text("Web Developer");
     }
-    if(e.key=="ArrowUp" || e.key=="PageUp"){
-        update_loc(-1);
+    else if(scroll>=350 & scroll<500){
+        $(".name").text("Computer Science Engineer");
     }
-});
-
-//update location
-
-function update_loc(i){
     prev_loc=loc;
-    if(i==-1){
-        loc--;
-        loc = Math.max(loc,0);
-    }
-    else{
-        loc++;
-        loc = Math.min(loc,7);
-    }
-    move_page(loc,prev_loc);
-}
-
-//movement function
-
-function move_page(loc, prev_loc){
-    if(loc>prev_loc){
-        if(prev_loc==0){
-            $(".top-container").slideUp();
-            $(".scroll").fadeOut();
-        }
-        if(prev_loc==1){
-            $(".name").slideUp();
-        }
-        if(prev_loc==2){
-            $(".tech").slideUp();
-        }
-        if(prev_loc==3){
-            $(".webdev").slideUp();
-        }
-        if(prev_loc==4){
-            $(".engineer").slideUp();
-        }
-        if(prev_loc==5){
-            $(".ai").slideUp();
-        }
-        if(prev_loc==6){
-            $(".middle-container").slideUp();
-        }
-    }
-    if(loc<prev_loc){
-        if(prev_loc==1){
-            $(".top-container").slideDown();
-            $(".scroll").fadeIn();
-        }
-        if(prev_loc==2){
-            $(".name").slideDown();
-        }
-        if(prev_loc==3){
-            $(".tech").slideDown();
-        }
-        if(prev_loc==4){
-            $(".webdev").slideDown();
-        }
-        if(prev_loc==5){
-            $(".engineer").slideDown();
-        }
-        if(prev_loc==6){
-            $(".ai").slideDown();
-        }
-        if(prev_loc==7){
-            $(".middle-container").slideDown();
-        }
-    }
 }
