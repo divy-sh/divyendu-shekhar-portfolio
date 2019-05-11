4399f5a2637a529e2e20856c4d2bfe948ae1469f
Divyendu Shekhar <divyendua@gmail.com>
2019-05-11 17:09:43 +0530

    .

--- script/index.js
+++ script/index.js
@@ -21,21 +21,21 @@ function track_movement(){
     if(scroll==0){
         $(".scroll").fadeIn();
     }
-    if(scroll>=0 & scroll<200){
+    if(scroll>=0 & scroll<100){
         $(".name").text("I am Divyendu");
         $(".expl").fadeOut();
 
     }
-    else if(scroll>=200 & scroll<400){
+    else if(scroll>=100 & scroll<300){
         $(".name").text("Tech Enthusiast");
         $(".expl").text("I love playing with new teach and always love it when something innovative comes in the market.");
         $(".expl").fadeIn();
     }
-    else if(scroll>=400 & scroll<600){
+    else if(scroll>=300 & scroll<500){
         $(".name").text("Web Developer");
         $(".expl").text("I can develop websites like this one. I am still learning the backend development though.");
     }
-    else if(scroll>=600 & scroll<800){
+    else if(scroll>=500 & scroll<700){
         $(".name").text("Computer Science Engineer");
         $(".expl").text("I am currently pursuing Engineering in Computer Science and expect to finish it by the end of 2019.");
     }
