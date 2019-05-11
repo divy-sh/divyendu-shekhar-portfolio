2b125ef3f4a4bce6556bc60b1c993b13803288ee
Divyendu Shekhar <divyendua@gmail.com>
2019-05-11 22:41:46 +0530

    .

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+function loop(){
+    $('.scroll').animate({ bottom : 10}, 'slow');
+    $('.scroll').animate({ bottom : 20}, 'slow');
+    loop();
+}
+
+loop();
+// website animate
+// show/hide top bar
+
 $(document).scroll(track);
 function track(){
     var e = $(document).scrollTop();
     if(e!=0){
         $(".top-bar").fadeOut();
-        console.log("up");
     }
     else{
         $(".top-bar").fadeIn();
-        console.log("down");
     }
 }
