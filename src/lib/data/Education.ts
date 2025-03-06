998757397f0977655724c65ebe91fb44c989c6fe
divyendu <divyendua@gmail.com>
2025-03-06 01:13:47 -0500

    add location for education

--- src/lib/data/Education.ts
+++ src/lib/data/Education.ts
@@ -3,12 +3,14 @@ const Education = [
 		school: 'Northeastern University',
 		degree: 'Master of Science; Computer Science',
 		date: 'Sep 2023 - May 2025',
+		location: 'Boston, USA',
 		description:
 			['Relevant Coursework: Program Design Paradigms, Database Management Systems, Algorithms.']
 	},
 	{
 		school: 'Sikkim Manipal University',
 		degree: 'B.Tech; Computer Science',
+		location: 'Gangtok, India',
 		date: 'Jul 2015 - Jul 2019',
 		description:
 			['Relevant Coursework: Operating Systems, Computer Networks, System Design, Software Engineering.']
