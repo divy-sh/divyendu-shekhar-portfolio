b6dcd28ddbf61274c456e1550429ca7ade402861
divyendu <divyendua@gmail.com>
2025-03-12 00:25:17 -0400

    minor updates

--- src/lib/data/Experiences.ts
+++ src/lib/data/Experiences.ts
@@ -4,26 +4,21 @@ const experiences = [
 		title: 'Software Engineer Co-op',
 		location: 'Boston, MA',
 		date: 'Jul 2024 - Dec 2024',
-		description: [`Developed and maintained full-stack solutions using C# .NET, Angular, and React, enhancing user 
-			experience and system performance for SS&C EZE's Portfolio Management, Accounting, and Trading solutions.`]
+		description: []
 	},
 	{
 		company: 'Clarivate',
 		title: 'Software Engineer',
 		date: 'Oct 2021 - Aug 2023',
 		location: 'Bangalore, India',
-		description: [`Contributed as a Backend Engineer within the platform team, specializing in developing Spring (Java) 
-			based microservices catering to user authentication, authorization, and identity management. Supported over 50 
-			business units across the organizational spectrum.`]
+		description: []
 	},
 	{
 		company: 'Clarivate',
 		title: 'Associate Software Engineer',
 		date: 'Nov 2019 - Oct 2021',
 		location: 'Bangalore, India',
-		description: [`Contributed as a Backend Engineer within the platform team, specializing in developing Spring (Java) 
-			based microservices catering to user authentication, authorization, and identity management. Supported over 50 
-			business units across the organizational spectrum.`]
+		description: []
 	},
 ];
 
