3e0cd1eb7a3e637c1ea83fb3b5ccc35a115b52dc
divyendu <divyendua@gmail.com>
2025-04-30 03:02:43 -0400

    minor updates

--- src/lib/data/Education.ts
+++ src/lib/data/Education.ts
@@ -4,12 +4,16 @@ const Education = [
 		degree: 'Master of Science; Computer Science',
 		date: 'Sep 2023 - May 2025',
 		location: 'Boston, USA',
+		description:
+			['Relevant Coursework: Program Design Paradigms, Database Management Systems, Algorithms, Cloud Computing.']
 	},
 	{
 		school: 'Sikkim Manipal University',
 		degree: 'B.Tech; Computer Science',
 		location: 'Gangtok, India',
 		date: 'Jul 2015 - Jul 2019',
+		description:
+			['Relevant Coursework: Operating Systems, Computer Networks, System Design, Software Engineering.']
 	},
 ];
 
--- src/lib/data/Experiences.ts
+++ src/lib/data/Experiences.ts
@@ -4,21 +4,24 @@ const experiences = [
 		title: 'Software Engineer Co-op',
 		location: 'Boston, MA',
 		date: 'Jul 2024 - Dec 2024',
-		description: []
+		description: [`Developed and maintained full-stack solutions using C# .NET, Angular, and React, enhancing user experience
+			and system performance for SS&C EZE’s Portfolio Management, Accounting, and Trading solutions.`]
 	},
 	{
 		company: 'Clarivate',
 		title: 'Software Engineer',
 		date: 'Oct 2021 - Aug 2023',
 		location: 'Bangalore, India',
-		description: []
+		description: [`Contributed as a Backend Engineer, specializing in Spring (Java) microservices catering to user authentication,
+			authorization, and identity management, used by over 50 business units across the organizational spectrum.`]
 	},
 	{
 		company: 'Clarivate',
 		title: 'Associate Software Engineer',
 		date: 'Nov 2019 - Oct 2021',
 		location: 'Bangalore, India',
-		description: []
+		description: [`Contributed as a Backend Engineer, specializing in Spring (Java) microservices catering to user authentication,
+			authorization, and identity management, used by over 50 business units across the organizational spectrum.`]
 	},
 ];
 
